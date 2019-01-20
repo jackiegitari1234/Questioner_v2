7c4c7b27df8adf23fa97c67945bcb19ebac2f172
jackie <mgjackline@gmail.com>
2019-01-20 18:28:35 +0300

    [Starts #163294622] edge cases

--- app/__init__.py
+++ app/__init__.py
@@ -1,46 +1,21 @@
 '''THIS IS THE GLUE'''
 
-#downloaded modules
+# downloaded modules
 from flask import Flask, jsonify
 
 # local imports
 from Instance.config import app_config
 from app.api.v2.views.auth_view import v2 as V2_auth
-
-#edge
-def invalid_method(error):
-    return jsonify({
-        'error': str(error),
-        'status': 405
-        }), 405
-
-#bad request
-def bad_request(error):
-    return jsonify({
-        'error': str(error),
-        'status': 400
-        }), 400
-
-#unexisting url
-def page_not_found(error):
-    return jsonify({
-        'error': str(error),
-        'status': 404
-        }), 404
-
-#internal server error
-def server_error(error):
-    return jsonify({
-        'error': str(error),
-        'status': 500
-        }), 500
-
+from app.api.v2.utils.errorhandlers import page_not_found,server_error,invalid_method,bad_request
+from app.api.v2.utils.database import init_db, create_tables
 
 
 def create_app(configName):
     app = Flask(__name__)
     app.config.from_object(app_config["development"])
     app.register_blueprint(V2_auth)
+    
+    create_tables()
 
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(500, server_error)
@@ -48,4 +23,3 @@ def create_app(configName):
     app.register_error_handler(400, bad_request)
 
     return app
-
