c9b533b3b7b45fecb2d95b888c73371e834c9776
jackie <mgjackline@gmail.com>
2019-01-22 12:24:39 +0300

    [Finishes #163294622] modify configurations

--- Instance/config.py
+++ Instance/config.py
@@ -5,7 +5,6 @@ import os
 class Config(object):
     '''Parent configuration class'''
     DEBUG = False
-    SECRET_KEY = 'tryandguess'
     ENV = 'development'
     SECRET = os.getenv('SECRET')
     DATABASE_URL = os.getenv("DB_DEVELOPMENT_URL")
