fb68572326e123cd817bf3e756c1f101c059f15d
jackie <mgjackline@gmail.com>
2019-01-23 17:09:19 +0300

    [Feature #163294779] jwt auth

--- run.py
+++ run.py
@@ -1,12 +1,19 @@
 '''ENTRY POINT OF THE APP'''
 # global builtin modules
 import os
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 # local imports
 from app import create_app
 
 config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
+SECRET_KEY = os.getenv("SECRET")
+app.config['JWT_SECRET_KEY'] = SECRET_KEY  # Change this!
+jwt = JWTManager(app)
 
 
 @app.route('/')
